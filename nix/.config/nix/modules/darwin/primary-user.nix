f882691671efcec8591545380d4a50e055a49979
Jurmarcus Allen <allenj@meta.com>
2025-09-23 11:27:31 -0700

    rip nix hype

--- nix/.config/nix/modules/darwin/primary-user.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, ... }:
-{
-  options.my.primaryUser = lib.mkOption {
-    type = lib.types.str;
-    default = "methylene";
-    description = "The primary user of the host";
-  };
-}
